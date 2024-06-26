4f60b213584c1dcf87e5c405d8b540da7058f2c4
hariom-cs <hariomcse01@gmail.com>
2024-06-26 15:55:47 +0530

    2 way of use of event module

--- StreamModule/index.js
+++ StreamModule/index.js
@@ -4,10 +4,27 @@ const http = require("http");
 const server = http.createServer();
 
 server.on("request",(req, res)=>{
+    //1st Way- this is not at all effective bq we download all the data and load to server
     fs.readFile("input.txt", (err, data)=>{
         if (err) return console.error(err);
         res.end(data.toString());
     });
+
+
+
+    // // 2nd way-- this is effective (it load  in stream manner)
+    // const rstream = fs.createReadStream("input.txt");
+
+    // rstream.on("data",(chunkdata)=>{
+    //     res.write(chunkdata);
+    // });
+    // rstream.on("end",()=>{
+    //     res.end();
+    // });
+    // rstream.on('error',(err)=>{
+    //     console.log(err);
+    //     res.end ("file not find");
+    // });
 });
 
 //to start server
